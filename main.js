e7a7658740148d5de7372aa7142550c414e01ad3
Josh <155296445+shojinPy@users.noreply.github.com>
2024-02-17 19:00:15 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+let userInput = document.querySelector('#textbox');
+const parentContainer = document.querySelector('.container');
+const addBtn = document.querySelector('#addBtn');
+
+function createTask(){
+    // Make task
+    const addTask = document.createElement('div');
+    addTask.textContent = userInput.value;
+    addTask.setAttribute('class', "task");
+    parentContainer.appendChild(addTask);
+
+    // Make trash button
+    const addTrashBtn = document.createElement('button');
+    addTrashBtn.textContent = "✖";
+    addTrashBtn.setAttribute('class','trashBtn')
+    addTask.appendChild(addTrashBtn)
+
+    // Delete button
+    addTrashBtn.onclick = function(){
+        parentContainer.removeChild(addTask);
+    }
+}
+
+addBtn.onclick = function(){
+    if(userInput.value===''){
+        alert("Task can't be blank.")
+    }
+    else{
+        createTask();
+        userInput.value = '';
+    }
+}
